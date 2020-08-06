8a5b11422a5bd79b023809d92e830cbcf38a8ada
jonaskay <jonaskay@iki.fi>
2020-08-06 09:40:10 +0300

    Update runtime

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy $CLOUD_FUNCTION_NAME --runtime=nodejs10 --region=$CLOUD_REGION --entry-point=main --trigger-topic=$PUBSUB_TOPIC_NAME
+gcloud functions deploy $CLOUD_FUNCTION_NAME --runtime=nodejs12 --region=$CLOUD_REGION --entry-point=main --trigger-topic=$PUBSUB_TOPIC_NAME
