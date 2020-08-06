a68c3c529e2829a52ff7eb6b0190eadb6befca7c
jonaskay <jonaskay@iki.fi>
2020-08-06 09:04:10 +0300

    Add entrypoint

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy $CLOUD_FUNCTION_NAME --trigger-topic $PUBSUB_TOPIC_NAME --env-vars-file .env.yaml --runtime nodejs12
+gcloud functions deploy $CLOUD_FUNCTION_NAME --runtime=nodejs12 --env-vars-file=.env.yaml --entry-point=main --trigger-topic=$PUBSUB_TOPIC_NAME
